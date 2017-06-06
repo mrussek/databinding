d76f43c0240a0486c5542b32e7136955ecf0ddc9
Max Russek <maxwell.russek@workday.com>
2017-06-06 17:57:57 -0600

    Remove unnecessary fragment.

--- app/src/main/java/com/mrussek/databinding/ArticleActivity.java
+++ app/src/main/java/com/mrussek/databinding/ArticleActivity.java
@@ -3,37 +3,42 @@ package com.mrussek.databinding;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class ArticleActivity extends AppCompatActivity {
     public static final String ARTICLE_TAG = "ARTICLE_TAG";
-    private final String ARTICLE_FRAGMENT_TAG = "Article Fragment";
-    private ArticleFragment articleFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_binding);
+        setContentView(R.layout.article_layout);
 
         ArticleEntity article = getArticleEntity(getIntent());
 
-        setUpArticleFragment(article);
+        bindArticle(article);
     }
 
-    private void setUpArticleFragment(ArticleEntity article) {
-        FragmentManager supportFragmentManager = getSupportFragmentManager();
-        articleFragment = (ArticleFragment)supportFragmentManager.findFragmentByTag(ARTICLE_FRAGMENT_TAG);
+    private void bindArticle(ArticleEntity article) {
+        // If only there was an easier way...
+        TextView articleTitle = (TextView) findViewById(R.id.article_title);
+        TextView mainAuthor = (TextView) findViewById(R.id.main_author);
+        TextView publishedDate = (TextView) findViewById(R.id.published_date);
+        View endorsedMarker = findViewById(R.id.endorsed_marker);
+        TextView numViews = (TextView) findViewById(R.id.num_views);
+        TextView articleContent = (TextView) findViewById(R.id.article_content);
+        TextView rating = (TextView) findViewById(R.id.rating);
 
-        if (articleFragment == null) {
-            articleFragment = ArticleFragment.newInstance(article);
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.add(R.id.fragment_container, articleFragment, ARTICLE_FRAGMENT_TAG);
-            transaction.commit();
-        }
+        articleTitle.setText(article.getTitle());
+        mainAuthor.setText(article.getAuthors().get(0));
+        publishedDate.setText(article.getPublishedDate().toString());
+        endorsedMarker.setVisibility(article.isEndorsed() ? View.VISIBLE : View.GONE);
+        numViews.setText(getString(R.string.num_of_views, article.getNumViews()));
+        articleContent.setText(article.getContent());
+        rating.setText(article.getRating().toString());
     }
+
 
     @NonNull
     private ArticleEntity getArticleEntity(Intent intent) {
